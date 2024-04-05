1abb87f3a54d293167cd5f007d2bb58b0874623a
hungtd <hungtdqn96@gmail.com>
2024-04-05 20:26:03 +0700

    update

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,20 +1,31 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { VueFire, VueFireAuth, VueFireAppCheck,  } from 'vuefire'
+import { boot } from 'quasar/wrappers';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDfZQPZoaIU263jBDMGBFim7o4lWuMNtAQ',
-  authDomain: 'quasar-mobile-template.firebaseapp.com',
-  projectId: 'quasar-mobile-template',
-  storageBucket: 'quasar-mobile-template.appspot.com',
-  messagingSenderId: '369233269964',
-  appId: '1:369233269964:web:b0cc193eae698d82801e69',
-  measurementId: 'G-RWXPF2DNWR'
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID'
 };
-
 
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
 export { firebaseApp, firebaseAuth, db };
+
+
+export default boot(({ app }) => {
+
+  app.use(VueFire, {
+    firebaseApp,
+    modules: [
+      VueFireAuth(),
+    ]
+  });
+});
